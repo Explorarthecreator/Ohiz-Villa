e138165d96c80ac8475aefac5e37c94e25d5320f
Explorarthecreator <fredadejiemma99@gmail.com>
2023-12-31 05:31:25 +0100

    Created the routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-
+import Homepage from "./Pages/Homepage";
+// import AboutPage from "./Pages/AboutPage"
+import Aboutpage from "./Pages/Aboutpage"
+import Notfound from "./Pages/Notfound";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-6xl text-black'>
-        Welcome
-      </h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar title="Ohiz Villa"/>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Homepage/>}/>
+            <Route path="/about" element={<Aboutpage/>}/>
+            <Route path="/notfound" element={<Notfound/>}/>
+            <Route path="/*" element={<Notfound/>}/>
+          </Routes>
+        </main>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
