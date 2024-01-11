80f54e236eaf0094441e6af12c0f9102afb9967b
Explorarthecreator <fredadejiemma99@gmail.com>
2024-01-11 12:35:59 +0100

    Set up the route and created a new route for the checkout page. Added the lodge provider and the room provider from the lodge and room context

--- src/App.js
+++ src/App.js
@@ -4,23 +4,33 @@ import Homepage from "./Pages/Homepage";
 import Aboutpage from "./Pages/Aboutpage"
 import Notfound from "./Pages/Notfound";
 import Navbar from "./components/layout/Navbar";
+import Lodge from "./Pages/Lodge";
 import Footer from "./components/layout/Footer";
+import Checkout from "./Pages/Checkout";
+import { LodgeProvider } from "./context/lodge/LodgeContext";
+import { RoomProvider } from "./context/room/RoomContext";
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar title="Ohiz Villa"/>
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Homepage/>}/>
-            <Route path="/about" element={<Aboutpage/>}/>
-            <Route path="/notfound" element={<Notfound/>}/>
-            <Route path="/*" element={<Notfound/>}/>
-          </Routes>
-        </main>
-        <Footer/>
-      </div>
-    </Router>
+    <LodgeProvider>
+      <RoomProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar title="Ohiz Villa"/>
+            <main className="container mx-auto px-3 pb-12">
+              <Routes>
+                <Route path="/" element={<Homepage/>}/>
+                <Route path="/about" element={<Aboutpage/>}/>
+                <Route path="/lodge/:lodgename" element={<Lodge/>}/>
+                <Route path="/notfound" element={<Notfound/>}/>
+                <Route path="/checkout" element={<Checkout/>}/>
+                <Route path="/*" element={<Notfound/>}/>
+              </Routes>
+            </main>
+            <Footer/>
+          </div>
+        </Router>
+      </RoomProvider>
+    </LodgeProvider>
   );
 }
 
