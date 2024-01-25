886129c74080228bbc7ac7bd20b44bf7923e601d
Explorarthecreator <fredadejiemma99@gmail.com>
2024-01-25 23:51:10 +0100

    Added the Toast container from toastify and also brought in the redirect page and added it to the routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 // import AboutPage from "./Pages/AboutPage"
+import { ToastContainer } from "react-toastify";
 import Aboutpage from "./Pages/Aboutpage"
 import Notfound from "./Pages/Notfound";
 import Navbar from "./components/layout/Navbar";
@@ -9,6 +10,8 @@ import Footer from "./components/layout/Footer";
 import Checkout from "./Pages/Checkout";
 import { LodgeProvider } from "./context/lodge/LodgeContext";
 import { RoomProvider } from "./context/room/RoomContext";
+import RedirectPaymentPage from "./Pages/RedirectPaymentPage";
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <LodgeProvider>
@@ -24,11 +27,24 @@ function App() {
                 <Route path="/notfound" element={<Notfound/>}/>
                 <Route path="/checkout" element={<Checkout/>}/>
                 <Route path="/*" element={<Notfound/>}/>
+                <Route path="/red?:params" element={<RedirectPaymentPage/>}/>
               </Routes>
             </main>
             <Footer/>
+            
           </div>
         </Router>
+        <ToastContainer position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick={true}
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            />
       </RoomProvider>
     </LodgeProvider>
   );
