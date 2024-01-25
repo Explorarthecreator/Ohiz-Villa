ed277c40c82f27216bb8bf4621c0820e25ad2295
Explorarthecreator <fredadejiemma99@gmail.com>
2024-01-25 23:57:10 +0100

    Needed to send out the set loading function

--- src/context/lodge/LodgeContext.js
+++ src/context/lodge/LodgeContext.js
@@ -62,6 +62,7 @@ export const LodgeProvider=({children})=>{
         lodge:state.lodge,
         // rom: state.lodge,
         fetchLodge,
+        setLoading,
         // setRa
     }}>
         {children}
