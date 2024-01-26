4d6d83a972b130330f3c21c39228ccd9c236f6a0
Explorarthecreator <fredadejiemma99@gmail.com>
2024-01-26 15:40:15 +0100

    Create this page to display and be abke toprint receipts. Used the react-to-print package here

--- src/Pages/RedirectPaymentPage.jsx
+++ src/Pages/RedirectPaymentPage.jsx
@@ -1,12 +1,31 @@
-import { useParams } from "react-router-dom"
+// import { useParams } from "react-router-dom"
+import { useRef } from "react"
+import {useReactToPrint} from "react-to-print"
+import { Printer } from "./Printer"
 function RedirectPaymentPage() {
-    const params = useParams()
+
+    const componentRef = useRef()
+    const handlePrint = useReactToPrint({
+      content: ()=> componentRef.current
+    })
+    // const params = useParams()
     // const params = new Url
   return (
     <div>
-        {
-            params.status
-        }
+      <p>
+        Payment was successful
+      </p>
+      <div className="overflow-x-auto">
+        {/* <ReactToPrint trigger={()=>{
+          return <button> Print Receipt</button>
+        }} content={()=> componentRef}/> */}
+
+        <Printer ref={componentRef}/>
+        <button onClick={handlePrint}>
+          Print Receipt
+        </button>
+        
+      </div>
     </div>
   )
 }
