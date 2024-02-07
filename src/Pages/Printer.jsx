488849673cf37ca391c63bae9eb1f50b896685e7
Explorarthecreator <fredadejiemma99@gmail.com>
2024-02-07 09:28:26 +0100

    style: Created the way the receipt would look like when it is about to be printed or about to be saved, depending on the scenario

--- src/Pages/Printer.jsx
+++ src/Pages/Printer.jsx
@@ -1,30 +1,68 @@
 import React, { useContext } from 'react'
-import RoomContext from '../context/room/RoomContext'
+import { FaCheckCircle } from 'react-icons/fa'
+// import RoomContext from '../context/room/RoomContext'
 import LodgeContext from '../context/lodge/LodgeContext'
 
 export const Printer = React.forwardRef((props,ref)=>{
-    const {rom} = useContext(RoomContext)
-    const {lodge} = useContext(LodgeContext)
+    // const {rom} = useContext(RoomContext)
+    const {receipt} = useContext(LodgeContext)
     return (
         
-        <div ref={ref} className=' text-center'>
-            
-        
+        <div ref={ref} className='flex justify-center items-center'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <FaCheckCircle color='#65B741' size={'5em'}/>    
+                </figure>
+                <div className="card-body">
+                    {/* <h2 className="card-title">
+                        Successfull
+                    </h2> */}
+                    
 
-            <p>
-                Welcome to this beatiful Building i love al; wed
-            </p>
-            <p>
-                Payment for room {rom.number}
-            </p>
-            <p>
-                {
-                    lodge.name
-                }
-            </p>
-            <p>
-                Payment Status: Successful
-            </p>
+                    <div className="overflow-x-auto">
+                        <table className="table">
+                            <tbody>
+                            {/* row 1 */}
+                            <tr>
+                                <th>
+                                    Lodge Name
+                                </th>
+                                <td>
+                                    {receipt.lodgeName}
+                                </td>
+                            </tr>
+                            {/* row 2 */}
+                            <tr>
+                                <th>
+                                    Room Number
+                                </th>
+                                <td>
+                                    {receipt.number}
+                                </td>
+                            </tr>
+                            {/* row 3 */}
+                            <tr>
+                                <th>
+                                    Price
+                                </th>
+                                <td>
+                                    {receipt.price}
+                                </td>
+                            </tr>
+                            {/* row 4 */}
+                            <tr>
+                                <th>
+                                    Status
+                                </th>
+                                <td>
+                                    Paid
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+</div>
+                </div>
+            </div>
         </div>        
       )
 })
